9a9f0b3844d95a1dae10d35f9007050d23c67710
JKlinkert <jklinkert@smu.edu>
2020-01-31 15:56:18 -0600

    Understanding how to write generic types for blackboard data types

--- Code/Game/Blackboard.cpp
+++ Code/Game/Blackboard.cpp
@@ -1,1 +1,12 @@
 #include "Game/Blackboard.hpp"
+
+void Blackboard::Add(const std::string& semantic_name, int num)
+{
+	m_testElement.first = semantic_name;
+	
+}
+
+void Blackboard::Remove(const std::string& semantic_name)
+{
+	
+}
